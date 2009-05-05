220afeb4be1cfcb09f731c7840fd0af49e9b5a07
jacobkwitkoski <jacobkwitkoski@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-05 22:11:15 +0000

    Hits the second elevator and does not go back too far.

--- trunk/Phase6.java
+++ trunk/Phase6.java
@@ -32,7 +32,7 @@ public class Phase6 {
         //turn to face the base
         ActionHelper.Rotate(75, SPEED, 0.5, "FACING BASE");
         //go backwards to hit the elevator
-        ActionHelper.MotorGo(-35, SPEED, -35, SPEED, "HITTING ELEVATOR");
+        ActionHelper.MotorGo(-30, SPEED, -30, SPEED, "HITTING ELEVATOR");
         //go to the nano base
         ActionHelper.MotorGo(80, SPEED, 80, SPEED, "GO TO NANO BASE");
     }
