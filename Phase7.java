82f0a047c719dc29f48179a27e9ce124d71f35a4
jacobkwitkoski <jacobkwitkoski@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-08 16:19:10 +0000

    Revised turning onto the ramp to go back to the power field

--- Phase7.java
+++ Phase7.java
@@ -32,15 +32,15 @@ public class Phase7 {
         // Turn left
         ActionHelper.Rotate(95, turn, 0.5);
         // Pushing lever
-        ActionHelper.MotorGo(35, speed, 35, speed, "PUSHING LEVER");
+        ActionHelper.MotorGo(30, speed, 30, speed, "PUSHING LEVER");
         // Reverse
         ActionHelper.MotorGo(-35, speed, -35, speed, "PLANTING TREES");
         // Turning left
         ActionHelper.Rotate(90, turn, 0.5);
         // Forward to ramp
-        ActionHelper.MotorGo(30, speed, 30, speed, "GOING TO RAMP");
+        ActionHelper.MotorGo(25, speed, 25, speed, "GOING TO RAMP");
         // Turn to line up with ramp
-        ActionHelper.Rotate(-90, turn, 0.5);
+        ActionHelper.Rotate(-95, turn, 0.5);
         // Go to power base
         ActionHelper.MotorGo(160, speed, 160, speed, "GOING TO POWER BASE");
     }
