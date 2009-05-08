23dc2bce9df818e94d52be266047a117df4deeb3
mikezobal <mikezobal@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-08 17:01:56 +0000

    added 2cm to initial move

--- Phase2.java
+++ Phase2.java
@@ -14,7 +14,7 @@ public class Phase2 {
         //turn right and leave base
         ActionHelper.Rotate(-90, speed, -2.75);
         //go towards corn
-        ActionHelper.MotorGo(46, speed, 46, speed, "GO TOWARDS CORN");
+        ActionHelper.MotorGo(48, speed, 48, speed, "GO TOWARDS CORN");
         //turn towads corn and gather
         ActionHelper.Rotate(-85, speed, 0);
         ActionHelper.MotorGo(17, speed, 17, speed, "GRAB CORN");
@@ -22,14 +22,14 @@ public class Phase2 {
         //scoop corn and face back to cart switch
         ActionHelper.Rotate(175, speed, 0);
         //back up to lever
-        ActionHelper.MotorGo(-11, speed, -11, speed, "REVERSE");
+        ActionHelper.MotorGo(-11, speed, -11, speed, "BASH THE SWITCH!");
         //flick lever
         ActionHelper.Rotate(-25, speed, 0.5);
         //head toward Uranium
-        ActionHelper.MotorGo(64, speed, 64, speed, "SCOOPING CORN");
+        ActionHelper.MotorGo(64, speed, 64, speed, "SCOOPING URANIUM");
         //scoop Uranium and point toward base
         ActionHelper.Rotate(150, 200, .5);
-//
+        //return to base
 //        ActionHelper.MotorGo(25, speed, 25, speed, "GOING BACK TO BASE");
 //        ActionHelper.Rotate(90, speed, 3);
 //        ActionHelper.MotorGo(20, speed, 20, speed, "GOING BACK TO BASE");
