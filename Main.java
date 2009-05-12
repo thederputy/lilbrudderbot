faf77a3c9754a5e27f43bdfeb7057f97cea585b4
FostinAux <FostinAux@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-12 23:33:24 +0000

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import lejos.nxt.Button;
+/**
+ *
+ * @author Austin
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("P1");
+        Button.waitForPress();
+        Phase1.main(args);
+        System.out.println("P2");
+        Button.waitForPress();
+        Phase2.main(args);
+        System.out.println("P3");
+        Button.waitForPress();
+        Phase3.main(args);
+        System.out.println("P4");
+        Button.waitForPress();
+        Phase4.main(args);
+        System.out.println("P5");
+        Button.waitForPress();
+        Phase5.main(args);
+        System.out.println("P6");
+        Button.waitForPress();
+        Phase6.main(args);
+        System.out.println("P7");
+        Button.waitForPress();
+        Phase7.main(args);
+    }
+}
