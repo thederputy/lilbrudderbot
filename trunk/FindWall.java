2f3b6e4ece307488b041f5ecb6d7838b93def0c3
jacobkwitkoski <jacobkwitkoski@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-19 17:52:14 +0000

    Added the hit satellite to Phase7

--- trunk/FindWall.java
+++ trunk/FindWall.java
@@ -13,16 +13,16 @@ import lejos.nxt.UltrasonicSensor;
  */
 public class FindWall {
     private static int threshold = 35;
-    private static LightSensor lt = new LightSensor(SensorPort.S3);
+    private static LightSensor lt = new LightSensor(SensorPort.S3, false);
     private static TouchSensor tc = new TouchSensor(SensorPort.S2);
     private static UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
     public static void TrackWall() {
         ActionHelper.Arc((int)(Math.random() * 360), 300, 0);
-        int dist = (int)(Math.random() * 10) + 5;
+        int dist = (int)(Math.random() * 10) + 20;
         ActionHelper.MotorGo(dist, 500, dist, 500);
         if (us.getDistance() < 10) {
             ActionHelper.MotorGo(20, 500, dist, 500);
-            ActionHelper.Arc(-90, 300, 0);
+            ActionHelper.Arc(90, 300, 0);
             if (lt.readValue() < threshold) {
                 Challenge3.foundWall();
             }
