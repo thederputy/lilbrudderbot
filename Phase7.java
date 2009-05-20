1c65cdb36322bf0d76aac7386701c9f2ba0378f1
jacobkwitkoski <jacobkwitkoski@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-20 16:08:41 +0000

    Added the hit of the second satellite lever.

--- Phase7.java
+++ Phase7.java
@@ -23,6 +23,11 @@ public class Phase7 {
          * <p>Speed of turns.</p>
          */
         int turn = 300;
+        /**
+         * <p>The speed when returning to the base after the second satellite
+         * hit.</p>
+         */
+        int back = 800;
         ActionHelper.Start();
         // Leaving base
         ActionHelper.MotorGo(55, speed, 55, speed, "LEAVING NANO BASE");
@@ -45,6 +50,17 @@ public class Phase7 {
         // Go to power base 
         ActionHelper.MotorGo(160, speed, 160, speed, "GOING TO POWER BASE");
         Button.waitForPress();
+        // Turn right to go towards the second satellite lever
+        ActionHelper.Rotate(-150, speed, 0.5, "TURNING RIGHT");
+        // Go forwards towards the second satellite lever
+        ActionHelper.MotorGo(60, speed, 60, speed, "GOING TOWARDS SATELLITE");
+        // Line up with the satellite
+        ActionHelper.Rotate(-30, speed, 0.5, "FACE SATELLITE");
         // Hit the satellite lever
+        ActionHelper.MotorGo(25, speed, 25, speed, "HIT THE SATELLITE");
+        // The following actions are a reverse of the going to satellite.
+        ActionHelper.MotorGo(-25, back, -25, back, "GO BACK");
+        ActionHelper.Rotate(30, back, 0.5, "GO BACK");
+        ActionHelper.MotorGo(-60, back, -60, back, "GO BACK");
     }
 }
