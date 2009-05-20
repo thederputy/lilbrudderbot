e3e7371b6880d9cb8971f0d2eb989431a74203cc
FostinAux <FostinAux@823f8d84-3526-11de-82fb-7f665ef830f7>
2009-05-20 15:40:33 +0000

    Improved volume

--- trunk/helper/IceThread.java
+++ trunk/helper/IceThread.java
@@ -37,7 +37,7 @@ public class IceThread extends Thread {
     }
 
     public void playTune() {
-        Sound.setVolume(11);
+        Sound.setVolume(100);
         for (int i = 0; i < chiptune.length; i += 2) {
             final int w = chiptune[i+1];
             final int n = chiptune[i];
